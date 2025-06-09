e9c258cfb411afc0e95baf1f2df9af9ac9864b63
Nayerlin <nayerlin.moreno0312@gmail.com>
2025-06-08 19:40:27 -0800

    run lint and build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import path from 'node:path';
+import netlify from '@netlify/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import netlify from '@netlify/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
